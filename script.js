e13df481f5b92a89fe101584f9799fe47f3f5226
Ayush Kumar <ayushk0520@gmail.com>
2021-12-31 17:33:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+	if(mode == 'light'){
+		document.getElementById('theme-style').href = 'default.css'
+	}
+
+	if(mode == 'blue'){
+		document.getElementById('theme-style').href = 'blue.css'
+	}
+	localStorage.setItem('theme', mode)
+}
